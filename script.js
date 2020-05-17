06e56fccb777701a676b60683fbb7420b8a9f0b5
cindy-harvard <62086685+cindy-harvard@users.noreply.github.com>
2020-05-16 17:45:46 -0700

    before attempting to switch to react

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// stick nav
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   if (scroll > 0) {
@@ -6,3 +7,33 @@ $(window).scroll(function () {
     $(".navbar").removeClass("active");
   }
 });
+
+// curtain nav
+function openNav() {
+  document.getElementById("navbar").style.width = "80%";
+}
+
+function closeNav() {
+  document.getElementById("navbar").style.width = "0%";
+}
+
+/* create function where if you you click outside
+the "myNav" id, you invoke the closeNav() function*/
+
+/*
+function closeNav2() {
+  var x = document.getElementById("myNav");
+  if (x.id !== "myNav") {
+    document.getElementById("myNav").style.width = "0%";
+  }
+}
+*/
+
+/*
+var modal = document.getElementById("myNav");
+window.onclick = function (event) {
+  if (event.target == modal) {
+    document.getElementById("myNav").style.width = "0%";
+  }
+};
+*/
